48e4dd17dd9cc4513668e6a7e983c4a4a89124f8
KILLER20071234 <kekics69@gmail.hu>
2020-11-29 10:37:10 +0100

    első változat

--- Hazi Feladat/2020-11-28/FreddyLogika/Program.cs
+++ Hazi Feladat/2020-11-28/FreddyLogika/Program.cs
@@ -7,13 +7,62 @@ namespace FreddyLogika
     {
         static void Main(string[] args)
         {
-            while (true)
+            //Console.WriteLine("írd be a nehézségi szintet");
+            //string nehezseg = Console.ReadLine();
+            //Convert.ToInt32(nehezseg);
+            bool jatekhossz = true;
+            
+
+
+            Random r = new Random();
+            
+
+            int freddyszam = r.Next(1, 6);
+            Console.WriteLine("Freddy másszkálós kedvében van:(");
+            int helységszám = 0;
+            while (jatekhossz==true)
             {
-                //
-                //
-                Thread.Sleep(1000);
-                Console.WriteLine("Freddy gondolkodik...");
+                int mozgasg = r.Next(1, 4);
+
+                helységszám = helységszám + 1;
+
+                if (helységszám == 1)
+                {
+                    Thread.Sleep(1 / freddyszam);
+                    Console.WriteLine("Freddy a szinpadon van MÉG");
+                }
+                else if (helységszám == 2 && mozgasg ==2)
+                {
+                    Thread.Sleep(5000 / freddyszam);
+                    Console.WriteLine("Freddy az étkezdében van");
+                }
+                else if (helységszám == 3 && mozgasg == 2)
+                {
+                    Thread.Sleep(5000 / freddyszam);
+                    Console.WriteLine("Freddynek vécéznie kell (Wcben van)");
+                }
+                else if (helységszám == 4 && mozgasg == 2)
+                {
+                    Thread.Sleep(5000 / freddyszam);
+                    Console.WriteLine("Freddy megéhezett MOST csak szendvicsre (konyhában van)");
+                }
+                else if (helységszám == 5 && mozgasg == 2)
+                {
+                    Thread.Sleep(5000 / freddyszam);
+                    Console.WriteLine("Freddy a folyosón van!!!!!!!!!:(");
+                }
+                else if (helységszám == 6 && mozgasg == 2)
+                {
+                    Thread.Sleep(5000 / freddyszam);
+                    Console.WriteLine("Melleted");
+                    jatekhossz = false;
+                }
+                else
+                {
+                    helységszám = helységszám - 1;
+                }
             }
+            Console.WriteLine("Játéknak vége");
         }
     }
 }
